ceee58a234dfdae1b97e64a7495dbad7efe97001
Javier Viesca <viesca@gmail.com>
2022-11-10 19:32:58 +0100

    Prueba

---
+++ js/script.js
@@ -0,0 +1,124 @@
+import{test} from "./test.js";
+
+window.onload = function () {
+
+    let nav = document.querySelector("nav");
+    let article = document.querySelector("article");
+    let inicio = document.querySelector(".inicio");
+
+    let divNav = document.createElement("div");
+    divNav.classList.add("nav");
+    
+
+    for (let categorias of test) {
+        let span = document.createElement("span");
+        span.textContent = categorias.categoria;
+        divNav.appendChild(span);
+        nav.appendChild(divNav);
+
+        //Función pinta CATEGORIA que seleccione
+        span.addEventListener("click", pinta);
+
+        
+        function pinta() {
+
+            //Pinta todos los nav del color original
+            for(let nav of this.parentElement.children){
+                nav.style.backgroundColor = "#29324183";
+            }
+            //Pinta el nav selecionado en color
+            this.style.backgroundColor = "#A46262";
+            
+
+            //Borra pagina de inicio
+            inicio.innerHTML = "";
+            //Borra preguntas anteriores
+            article.innerHTML = "";
+
+            //header
+            let headerArticle = document.createElement("div");
+            headerArticle.classList.add("header-article");
+            //img
+            let icono = document.createElement("img");
+            icono.src = `./images/${categorias.imgico}`;
+            icono.height="80";
+
+            //h1
+            let h1 = document.createElement("h1");
+            h1.textContent = categorias.categoria;
+            headerArticle.appendChild(h1);
+            headerArticle.appendChild(icono);
+            
+            article.appendChild(headerArticle)
+                                
+            //Cambia backgroundImage de body segun la categoria.Imagen de JSON
+            document.body.style.backgroundImage= `url('./images/${categorias.imgbody}')`;
+            
+            //Contador para atributo "name" de preguntas
+            let contadorPreg = 1;
+            for (let pregunt of categorias.preguntas) {
+                let divPreg = document.createElement("div");
+                divPreg.classList.add("pregunta");
+
+                let divTituloPreg = document.createElement("div");
+                divTituloPreg.classList.add("pregunta-titulo");
+
+                let spanH3 = document.createElement("span");
+                spanH3.textContent=contadorPreg;
+                divTituloPreg.appendChild(spanH3);
+
+                let h3 = document.createElement("h3");
+                h3.textContent = pregunt.pregunta;
+                divTituloPreg.appendChild(h3);
+
+                divPreg.appendChild(divTituloPreg);
+    
+    
+                //Contador para atributo "id" y "for" de type radio y label
+                let contadorResp = 1;
+                for (let resp of pregunt.respuestas) {
+                    let type = document.createElement("input");
+                    let divResp = document.createElement("div");
+                    divResp.classList.add("respuesta");
+                    type.type = pregunt.tipo == "simple" ? "radio" : "checkbox";
+                    type.name = `pregunta${contadorPreg}`;
+                    type.id = `respuesta${contadorPreg}${contadorResp}`;
+                    type.value = resp.correcta;
+                    let label = document.createElement("label");
+                    label.setAttribute("for", `respuesta${contadorPreg}${contadorResp}`);
+                    label.textContent = resp.respuesta;
+                    divResp.appendChild(type);
+                    divResp.appendChild(label);
+                    divPreg.appendChild(divResp);
+                    contadorResp++;
+                };
+
+                //Añadimos cada DIV de preguntas 
+                article.appendChild(divPreg);
+                contadorPreg++;
+            };
+    
+            //Creación del boton de corregir
+            let button = document.createElement("button");
+            let divButton = document.createElement("div");
+            divButton.classList.add("divButton");
+            button.textContent = "Corrige";
+            button.classList.add("boton");
+            divButton.appendChild(button);
+            article.appendChild(divButton);
+    
+            //QuerySelector Boton de Corregir
+            document.querySelector(".divButton").addEventListener("click", comprueba);
+        };
+    };
+
+
+    function comprueba() {
+        let radios = document.querySelectorAll("input[type]:checked");
+
+        for (let r of radios) {
+            r.nextElementSibling.style.backgroundColor = r.value == "true" ? "rgba(32, 108, 32, 0.486)" : "rgba(171, 19, 19, 0.492)";
+            r.nextElementSibling.style.borderRadius = "10px";
+        };
+    };
+};
---
+++ js/test.js
@@ -0,0 +1,382 @@
+let test = [
+    /*Categoría Ciencia*/
+    {
+        "categoria": "Ciencia",
+        "imgbody" : "Ciencia.webp",
+        "imgico" : "icoCiencia.png",
+        "preguntas": [
+            {
+                "pregunta": "¿Como se llama un animal que come plantas y otros animales?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Hervíboro",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Carnívoro",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Omnívoro",
+                        "correcta": "true"
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuál de los siguientes es un factor abiótico?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Roca",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Cactus",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Hongo",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿En qué estado de la materia están mas apretadas las partículas?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Sólido",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Líquido",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Gas",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuantos polos tiene un imán y en qué dirección están?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "1 polo que va al norte",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "2 polos que van al norte y al sur",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "1 polo que va al sur",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuál de estos es un recurso renovable?",
+                "tipo": "multiple",
+                "respuestas": [
+                    {
+                        "respuesta": "Oro",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Aluminio",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Madera",
+                        "correcta": "true"
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Qué planeta no tiene anillos?",
+                "tipo": "multiple",
+                "respuestas": [
+                    {
+                        "respuesta": "Plutón",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Urano",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Júpiter",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuánto tiempo tarda la Luna en dar una vuelta completa alrededor de la Tierra?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "31 días",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "27 días",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "29 días",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cómo se llama el proceso por el cual el agua pasa de estado liquido a gaseoso?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Condensación",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Precipitación",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Evaporación",
+                        "correcta": "true"
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuál de estas no es una parte de las células de un animal?",
+                "tipo": "multiple",
+                "respuestas": [
+                    {
+                        "respuesta": "Vacuolas",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Pared celular",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Mitocondria",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuantos días tarda la tierra en dar una vuelta al sol?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "250",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "395",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "365",
+                        "correcta": "true"
+                    }
+                ]
+            }
+        ]
+    },
+
+    /*Categoría Geografía*/
+    {
+        "categoria": "Geografía",
+        "imgbody":"Geografía.webp",
+        "imgico":"icoGeografía.png",
+        "preguntas": [
+            {
+                "pregunta": "¿En que país NO se localiza la ciudad de Hamburgo?",
+                "tipo": "compuesta",
+                "respuestas": [
+                    {
+                        "respuesta": "Bélgica",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Hungría",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Alemania",
+                        "correcta": "false"
+                    }
+                ]
+            },
+            {
+                "pregunta": "El río de la Plata hace frontera entre...",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Brasil y Paraguay",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Argentina y Uruguay",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Perú y Bolivia",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuál es el gentilicio de Yemen?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Yemení",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Yemeno",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Yemenio",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuál es la ciudad más grande de China?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Beijing",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Shanghái",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Hong Kong",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Qué cordillera separa Europa de Asia?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Montes Yáblonoi",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Himalaya",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Montes Urales",
+                        "correcta": "true"
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿En qué Oriente se ubica Yemen?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Próximo Oriente",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Medio Oriente",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Extremo Oriente",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuál es el gentilicio de la ciudad de Tokio?",
+                "tipo": "simple",
+                "respuestas": [
+                    {
+                        "respuesta": "Tokino",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Tokiota",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Tokinense",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿Cuál de estos países NO es centroamericano?",
+                "tipo": "multiple",
+                "respuestas": [
+                    {
+                        "respuesta": "USA",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Panamá",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Colombia",
+                        "correcta": "true"
+                    }
+                ]
+            },
+            {
+                "pregunta": "Santo Domingo es la capital de...?",
+                "tipo": "multiple",
+                "respuestas": [
+                    {
+                        "respuesta": "Puerto Rico",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Republica Dominicana",
+                        "correcta": "true"
+                    },
+                    {
+                        "respuesta": "Costa Rica",
+                        "correcta": ""
+                    }
+                ]
+            },
+            {
+                "pregunta": "¿En qué isla Canaria se encuentra La Caldera de Taburiente?",
+                "tipo": "multiple",
+                "respuestas": [
+                    {
+                        "respuesta": "La Gomera",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "Gran Canaria",
+                        "correcta": ""
+                    },
+                    {
+                        "respuesta": "La Palma",
+                        "correcta": "true"
+                    }
+                ]
+            }
+        ]
+
+    }
+];
+
+export{test};
